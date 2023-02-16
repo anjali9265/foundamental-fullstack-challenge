d1e975ea0531dccab0e489e603672fb7c273339e
ANJALI PA <anjali9265@gmail.com>
2023-02-16 17:11:25 +0100

    Configure db connection

--- api/fullstack_challenge_api/routes/companies.py
+++ api/fullstack_challenge_api/routes/companies.py
@@ -1,10 +1,42 @@
 from fastapi import APIRouter, Depends
 from fullstack_challenge_api.utils.db import get_db
 from sqlalchemy.orm import Session
+import sqlalchemy
+from sqlalchemy import create_engine
+import config
+# from databases import Database
 
 router = APIRouter()
+DATABASE_URL = "mysql+mysqldb://root:PASSWORD@localhosthost/app"
 
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = Session(autocommit=False, autoflush=False, bind=engine)
+# database = Database(DATABASE_URL)
+Base = declarative_base()
+Base.metadata.create_all(bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+class Company(Base):
+    __tablename__ = "challenge_companies"
+
+    key = Column(Integer, primary_key=True, index=True)
+    properties_name = Column(String)
+    properties_country = Column(String)
+    properties_founding_date = Column(Date)
+    properties_description = Column(String)
+    properties_company_id = Column(Integer)
 
 @router.get("/companies")
 async def get_companies(db: Session = Depends(get_db)):
-    pass
+      query = db.query(Company).all()
+      return query
+
+
+
